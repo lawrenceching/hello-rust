ae5d9c43e05192134b8971019eccfd44e246bc13
Lawrence Ching <lawrence.ching.zh@gmail.com>
2024-08-23 00:09:00 +0800

    Use custom source

--- run-without-crate-io-access.sh
+++ run-without-crate-io-access.sh
@@ -5,4 +5,8 @@ cat /build/hosts >> /etc/hosts
 echo "updated /etc/hosts"
 cat /etc/hosts
 
+echo "cargo version:"
+cargo version --verbose
+
+echo "start to cargo build"
 CARGO_HTTP_DEBUG=true cargo build
